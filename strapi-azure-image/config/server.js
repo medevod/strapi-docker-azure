a577e2d7e60795f852538ac4a8dfd8fc8dbc3c5e
Mehdi Oueldi <mehdi.oueldi@be-mo.io>
2021-11-26 07:47:17 +0100

    inject more  custom options from env

--- strapi-azure-image/config/server.js
+++ strapi-azure-image/config/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
   host: env("SERVER_HOST", "0.0.0.0"),
   port: env.int("SERVER_PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", ""),
+    },
+  },
 });
